9ab978614314c1f8f6ec6624a8ea5774ac28e7f1
SohailAman <101714964+SohailAman@users.noreply.github.com>
2022-11-26 23:56:38 +0500

    Adding Slider

--- js/sidebarv2.js
+++ js/sidebarv2.js
@@ -1,1 +1,12 @@
-// Project not accepting anonymous feedback
+let menuTrigger = document.getElementById('menuTrigger');
+let closeBtn = document.getElementById('closeBtn');
+let menuSlide = document.getElementById('menuSlide');
+
+menuTrigger.addEventListener('click', () => {
+    menuSlide.classList.add('slideInn');
+    menuSlide.classList.remove('slideOut');
+})
+closeBtn.addEventListener('click', () => {
+    menuSlide.classList.remove('slideInn');
+    menuSlide.classList.add('slideOut');
+})
